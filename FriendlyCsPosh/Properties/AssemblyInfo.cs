09901deb56981ebd98549f3f0ef0a783d863c652
3lpsy <vanguard@garden.kongroo.io>
2020-04-21 20:16:01 -0500

    init

--- FriendlyCsPosh/Properties/AssemblyInfo.cs
+++ FriendlyCsPosh/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CsPosh")]
+[assembly: AssemblyTitle("FriendlyCsPosh")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CsPosh")]
+[assembly: AssemblyProduct("FriendlyCsPosh")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
